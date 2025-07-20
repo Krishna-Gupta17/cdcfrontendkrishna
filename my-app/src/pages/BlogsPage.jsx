71eef906ddf282ee293b9105b3136476085e9671
Krishna-Gupta17 <2024021228@mmmut.ac.in>
2025-07-20 22:28:14 +0530

    BLOG SERVER URL CORRECTED

--- my-app/src/pages/BlogsPage.jsx
+++ my-app/src/pages/BlogsPage.jsx
@@ -13,7 +13,7 @@ const Blog = () => {
     description: '',
   });
   useEffect(() => {
-    axios.get(`http://127.0.0.1:4200/members/blog/${id}`)
+    axios.get(`${import.meta.env.VITE_SERVER_URL}/members/blog/${id}`)
       .then(res => setBlogData(res.data.data))
       .catch(err => console.error(err));
   }, [id]);
